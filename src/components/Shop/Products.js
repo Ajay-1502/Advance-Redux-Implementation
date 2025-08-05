1f21867d032d6f798c7ce3119ddd88baf2c9478c
Ajay-1502 <ahdesai40@gmail.com>
2025-08-05 11:46:10 +0530

    Feat : Cart functionality like add,remove,increment,  decrement  using redux-toolkit

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -2,15 +2,32 @@ import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
 const Products = (props) => {
+  const dummyProducts = [
+    {
+      id: 'p1',
+      price: 200,
+      title: 'Book 1',
+      description: 'The first book i read',
+    },
+    {
+      id: 'p2',
+      price: 300,
+      title: 'Book 2',
+      description: 'The second book i read',
+    },
+  ];
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {dummyProducts.map((product) => (
+          <ProductItem
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
